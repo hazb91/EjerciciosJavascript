5d2f67b36a3b3b0b1afdf8068c8db776de1b78d4
hazb <hectorazb@gmail.com>
2022-06-03 10:26:54 -0500

    Se completo el Ejercicio 1 y se planteo el 2

---
+++ JS/main.js
@@ -0,0 +1,32 @@
+// Ejercicio 1
+/*Escribir una función que tome un arreglo de números y devuelva el número 
+mayor de la lista. Ejemplo del arreglo : [3, 5, 7, 1, 6]*/
+
+let num1 = [3, 5, 7, 1, 6];
+console.log(Math.max(...num1));
+
+// Ejercicio 2
+/*Dada una lista de números [-1, 3, 4, 2, 6], escribe una función en 
+javascript para calcular el número más pequeño.*/
+
+function min (num2) {
+    num2 = [-1, 3, 4, 2, 6];
+    (Math.min(...num2));
+}
+console.log (min)
+
+//Ejercicio 3
+
+/*Escribir una función que permita saber si un número se repite dentro de un
+arreglo.
+*Casos de prueba*:
+Caso 1: Resultado esperado: El número 5 se encuentra más de una vez
+[1, 22, 5, 17, 10, 5, 40, 5]
+Caso 2: Resultado esperado: El número 7 se encuentra más de una vez
+[7, 41, 5, 7, 10, 13, 2]
+Caso 3: Resultado esperado: No hay números repetidos
+[1, 22, 5, 14, 24, 31, 27, 15, 105]*/
+
+
+
+
